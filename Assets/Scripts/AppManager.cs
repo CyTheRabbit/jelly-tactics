a154c017290c3b6731e1f0e51ed09abf0cc2b939
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-02-26 23:45:46 +0300

    Added application manager

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Application's entry point that initializes all core managers and sets
+/// some execution constants.
+/// </summary>
+public class AppManager : ScriptableObject
+{
+    private void Init()
+    {
+#if UNITY_EDITOR
+        Application.targetFrameRate = 30;
+#endif
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void EntryPoint()
+    {
+        const string appPath = "App";
+        var app = Instantiate(Resources.Load<AppManager>(appPath));
+        app.Init();
+    }
+}
