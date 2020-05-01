1eae2a9cb2fb36de6a718b73d7a17430fb49f41d
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-05-01 21:35:41 +0300

    Improved Patrol AI

--- Assets/Scripts/Enemies/AI/PatrolAI.cs
+++ Assets/Scripts/Enemies/AI/PatrolAI.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Enemies.AI
@@ -6,15 +8,29 @@ namespace Enemies.AI
     public class PatrolAI : MonoBehaviour
     {
         [SerializeField] private RouteMover m_mover = null;
+        
+        [Serializable]
+        private class Place
+        {
+            [SerializeField] private float m_position = 0.5f;
+            [SerializeField] private float m_wait = 0.5f;
+
+            public float Position => m_position;
+            public float Wait => m_wait;
+        }
+
+        [SerializeField, ReorderableList] private Place[] m_places = null;
 
         private IEnumerator MoveCoroutine()
         {
             while (true)
             {
-                m_mover.Target = 0;
-                yield return new WaitUntil(() => m_mover.Halt);
-                m_mover.Target = 1;
-                yield return new WaitUntil(() => m_mover.Halt);
+                foreach (Place place in m_places)
+                {
+                    m_mover.Target = place.Position;
+                    yield return new WaitUntil(() => m_mover.Halt);
+                    yield return new WaitForSeconds(place.Wait);
+                }
             }
         }
 
