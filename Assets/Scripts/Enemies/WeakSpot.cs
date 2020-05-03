fd5419da51a2783c5ec3132d1093f760492c71c6
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-05-03 23:34:56 +0300

    Fixed unable to hit weak spot when crab idle

--- Assets/Scripts/Enemies/WeakSpot.cs
+++ Assets/Scripts/Enemies/WeakSpot.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace Enemies
 {
     public class WeakSpot : MonoBehaviour
     {
-        public interface IListener : IEventSystemHandler
+        public interface IListener
         {
             void OnWeakSpotHit(Collider collision);
         }
         
         private void OnTriggerEnter(Collider other)
         {
-            ExecuteEvents.ExecuteHierarchy<IListener>(transform.parent.gameObject, null,
-                (listener, _) => listener.OnWeakSpotHit(other));
+            GetComponentInParent<IListener>().OnWeakSpotHit(other);
         }
     }
 }
