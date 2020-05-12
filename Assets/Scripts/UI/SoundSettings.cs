bea0fa515b31463a54f67988d5bbae03dd866110
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-05-13 01:57:24 +0300

    Fixed audio stuff

--- Assets/Scripts/UI/SoundSettings.cs
+++ Assets/Scripts/UI/SoundSettings.cs
@@ -15,21 +15,32 @@ namespace UI
         [SerializeField] private Image m_musicButton = null;
         [SerializeField] private Sprite m_musicOnSprite = null;
         [SerializeField] private Sprite m_musicOffSprite = null;
+        [Space]
+        [SerializeField] private float m_soundVolume = 0;
+        [SerializeField] private float m_musicVolume = 0;
 
         private bool soundOn = true;
         private bool musicOn = true;
 
+
+        private void Start()
+        {
+            m_soundMixer.SetFloat("Sound Volume", m_soundVolume);
+            m_soundMixer.SetFloat("Music Volume", m_musicVolume);
+        }
+
+
         public void ToggleSound()
         {
             soundOn = !soundOn;
-            m_soundMixer.SetFloat("Sound Volume", soundOn ? 0 : -80);
+            m_soundMixer.SetFloat("Sound Volume", soundOn ? m_soundVolume : -80);
             m_soundButton.sprite = soundOn ? m_soundOnSprite : m_soundOffSprite;
         }
 
         public void ToggleMusic()
         {
             musicOn = !musicOn;
-            m_soundMixer.SetFloat("Music Volume", musicOn ? 0 : -80);
+            m_soundMixer.SetFloat("Music Volume", musicOn ? m_musicVolume : -80);
             m_musicButton.sprite = musicOn ? m_musicOnSprite : m_musicOffSprite;
         }
     }
