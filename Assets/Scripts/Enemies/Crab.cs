3cbd8dfb711080d4554e61d14cd0afa85cffee11
Cyrill Podrez <CyTheRabbit@gmail.com>
2020-05-13 04:20:01 +0300

    Hardcoded crab animation

--- Assets/Scripts/Enemies/Crab.cs
+++ Assets/Scripts/Enemies/Crab.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Enemies
@@ -6,9 +7,15 @@ namespace Enemies
     {
         private void Perish()
         {
-            // TODO: Add animation
-            gameObject.SetActive(false);
-            Destroy(this);
+            foreach (Collider col in GetComponentsInChildren<Collider>()) col.enabled = false;
+            DOTween.Sequence()
+                .Append(transform.DOJump(transform.position + Vector3.down, 2, 1, 0.75f))
+                .Join(transform.DORotate(Vector3.forward * 240, 0.5f, RotateMode.LocalAxisAdd))
+                .AppendCallback(() =>
+                {
+                    gameObject.SetActive(false);
+                    Destroy(this);
+                });
         }
 
         public void OnWeakSpotHit(Collider other)
